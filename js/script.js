9a859e59b6e3187a6742d8e877d624a32079d531
johnbarsigo <john.barsigo@student.moringaschool.com>
2026-04-09 23:16:37 +0300

    Added script.js file in js folder. Linked to index.html.

---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+// SCROLL SECTIONS ACTIVE LINK
+
+const sections = document.querySelectorAll('section[id]')
+
+window.addEventListener('scroll', () => {
+    let current = '';
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+        if (pageYOffset >= sectionTop - 100) {
+            current = section.getAttribute('id');
+        }
+    });
+});
