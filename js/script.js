b55944dad59c81eb44149652c9a5644d68847a8e
john.barsigo@student.moringaschool.com <john.barsigo@student.moringaschool.com>
2026-04-13 13:05:05 +0300

    Fixed navbar active links on scroll.

--- js/script.js
+++ js/script.js
@@ -1,18 +1,38 @@
 
 // SCROLL SECTIONS ACTIVE LINK
 
-const sections = document.querySelectorAll('section[id]')
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
 
-window.addEventListener('scroll', () => {
-    let current = '';
-    sections.forEach(section => {
-        const sectionTop = section.offsetTop;
-        const sectionHeight = section.clientHeight;
-        if (pageYOffset >= sectionTop - 100) {
-            current = section.getAttribute('id');
-        }
+window.onscroll = () => {
+
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if ( top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+
+        };
     });
-});
+};
+
+
+// window.addEventListener('scroll', () => {
+//     let current = '';
+//     sections.forEach(section => {
+//         const sectionTop = section.offsetTop;
+//         const sectionHeight = section.clientHeight;
+//         if (pageYOffset >= sectionTop - 100) {
+//             current = section.getAttribute('id');
+//         }
+//     });
+// });
 
 
 // Mobile menu toggle
